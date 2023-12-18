991f613ba108cfc52c8666e6ad8053f3b0caa17e
tom <tom.desalmand@epitech.eu>
2023-12-18 18:20:00 +0100

    feat(client): Added all files for a basic client implementation

---
+++ client/include/textures.hpp
@@ -0,0 +1,19 @@
+#ifndef TEXTURES_HPP_
+#define TEXTURES_HPP_
+
+#include "includes.hpp"
+
+class textures {
+protected:
+    Texture _parallax;
+    Texture _ships;
+    Texture _planet;
+public:
+    textures();
+    Texture &getParallaxTexture();
+    Texture &getShipsTexture();
+    Texture &getPlanetTexture();
+    ~textures();
+};
+
+#endif /* !TEXTURES_HPP_ */
