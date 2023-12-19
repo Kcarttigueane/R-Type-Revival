7e997831459fd0569e2cae7f3e8ea2ab798a987a
tom <tom.desalmand@epitech.eu>
2023-12-19 17:26:29 +0100

    featt(loadAssetsClients): Implement dynamic path to load assets for the clients

---
+++ client/src/textures.cpp
@@ -0,0 +1,21 @@
+#include "../include/game.hpp"
+
+textures::textures(string assetsPath) {
+    _parallax.loadFromFile(assetsPath + "/Background/Layer 3/Space_background.png");
+    _ships.loadFromFile(assetsPath + "/Player/spaceship.gif");
+    _planet.loadFromFile(assetsPath + "/Background/Layer 1/Wet.png");
+}
+
+Texture &textures::getParallaxTexture() {
+    return _parallax;
+}
+
+Texture &textures::getShipsTexture() {
+    return _ships;
+}
+
+Texture &textures::getPlanetTexture() {
+    return _planet;
+}
+
+textures::~textures() {}
