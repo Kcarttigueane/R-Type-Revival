98ac385a5eb53d8f82dc1bf4be9d845ae9302445
jvlxz <jules.dutel@epitech.eu>
2024-01-08 09:28:27 +0100

    feat: installation script for linux

---
+++ install_linux.sh
@@ -0,0 +1,18 @@
+./vcpkg/bootstrap-vcpkg.sh
+
+./vcpkg/vcpkg integrate install
+./vcpkg/vcpkg install
+
+if [ -d "build" ]; then
+  echo "Build folder exists. Clearing contents..."
+  rm -rf build/*
+else
+  echo "Build folder does not exist. Creating..."
+  mkdir build
+fi
+
+cd build
+
+cmake ..
+
+make
