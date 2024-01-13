95f1eaaa977b5c50230d0843fb9af2215259eeab
CARTTIGUEANE Kévin <carttigueane.k@hotmail.com>
2024-01-13 13:36:12 +0100

    refactor: format code

--- client/src/utils/get_direction.cpp
+++ client/src/utils/get_direction.cpp
@@ -1,6 +1,7 @@
 #include "../../include/utils.hpp"
 
-std::pair<double, double> get_direction(float x1, float y1, float x2, float y2) {
+std::pair<double, double> get_direction(float x1, float y1, float x2, float y2)
+{
     float dx = x2 - x1;
     float dy = y2 - y1;
     float distance = std::sqrt(dx * dx + dy * dy);
